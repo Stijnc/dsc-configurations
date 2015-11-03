4fe4e8181f1bdbbd4f41237aa85be92b011964e7
Stijn Callebaut <stijnc@fooji.net>
2015-11-03 21:13:48 +0100

    added a node configuration to install packages
    register a chocolatey source, and install some modules. the nugetpackage
    config will fail due to a hardcoded provider in the resource

--- packageManagement/PackageManagement.ps1
+++ packageManagement/PackageManagement.ps1
@@ -25,4 +25,44 @@ Configuration PackageManagement {
         }
 
     }
+
+    Node installPackage {
+
+        PackageManagementSource SourceRepoChocolatey {
+            
+            Ensure = "Present"
+            Name = "Chocolatey"
+            ProviderName = "Chocolatey"
+            SourceUri = "http://chocolatey.org/api/v2/"
+            InstallationPolicy = "Untrusted"
+
+        }
+<#
+ The nugetpackage has the provider hardcoded to nuget. So setting the source to Chocolatey will fail anyhow
+ TODO: update the PackageManagementProviderResource module with a ChocolateyPackage resource 
+        NugetPackage Notepadplusplus {
+            Ensure = "Present"
+            Name = "notepadplusplus"
+            Source = "Chocolatey"
+            DependsOn = "[PackageManagementSource]SourceRepoChocolatey"
+            DestinationPath = "$env:ProgramFiles\notepadplusplus"
+            InstallationPolicy = "trusted"
+        }
+#>
+        PSModule Pester {
+            Ensure = "Present"
+            Name = "Pester"
+            Repository = "PSGallery"
+            InstallationPolicy="trusted"
+        }
+
+        PSModule xNetworking {
+            Ensure = "Present"
+            Name = "xNetworking"
+            Repository = "PSGallery"
+            InstallationPolicy="trusted"
+        }
+
+        
+    }
 }
