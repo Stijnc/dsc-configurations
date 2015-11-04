caa45d0d11a3c404bd6e0591fa34d2d72e8c932d
Stijn Callebaut <stijnc@fooji.net>
2015-11-04 20:44:57 +0100

    added logo

--- SCSM/SCSM.ps1
+++ SCSM/SCSM.ps1
@@ -1,15 +1,63 @@
 ﻿Configuration SCSM {
 
+    Import-DscResource -ModuleName xPSDesiredStateConfiguration, PSDesiredStateCongfi
     
     Node Portal {
         #install windows features
+        WindowsFeature webServerIIS
+        {
+            Ensure = 'Present'
+            Name = ''
+        }
 
+        WindowsFeature aspNet45 {
+            Ensure = 'Present'
+            Name = ''
+        }
+
+        WindowsFeature httpActivation {
+            Ensure = 'Present'
+            Name = ''
+        }
+
+        WindowsFeature dotNet35 {
+            Ensure = 'Present'
+            Name = ''
+        }
+
+        WindowsFeature basicAuthentication {
+            Ensure = 'Present'
+            Name = ''
+        }
+
+        WindowsFeature windowsAuthentication {
+            Ensure = 'Present'
+            Name = ''
+        }
+
+        WindowsFeature netExtensibility45 {
+            Ensure = 'Present'
+            Name = ''
+        }
+
+        WindowsFeature asp {
+            Ensure = 'Present'
+            Name = ''
+        }
+        WindowsFeature aspNet452 {
+            Ensure = 'Present'
+            Name = ''
+        }
+
+        File tmpFolder {
+            Ensure = 'Present'
+            Type = 'Directory'
+            DestinationPath = "$env:Systemdrive\tmp"
+        }
         #install package
 
-        #create temp folder --> for logging
-
         #copy logo
-
+        xRemoteFile 
         #AppSettings
 
 
